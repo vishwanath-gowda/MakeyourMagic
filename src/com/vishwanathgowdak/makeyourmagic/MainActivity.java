f98b0485af6ab11023ce31abe1a78acaf42a813c
D <naveend@hpe.com>
2016-04-13 15:52:10 +0530

    lable changes

--- src/com/vishwanathgowdak/makeyourmagic/MainActivity.java
+++ src/com/vishwanathgowdak/makeyourmagic/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
+    /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
@@ -37,7 +37,7 @@ public class MainActivity extends ActionBarActivity {
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
     
     public void sendMessage(View view) {
     	Intent intent = new Intent(this, DisplayMessageActivity.class);
